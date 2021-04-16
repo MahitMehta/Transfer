c0e2db5ef5388f007a8ac35eeee58e2ad4e9102d
MahitMehta <73650235+MahitMehta@users.noreply.github.com>
2021-04-15 23:02:29 -0400

    Port Fix

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.get('*', (_, res) => {
     res.sendFile(__dirname + "\\client\\build\\index.html")
 })
 
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
