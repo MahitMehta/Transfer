428c0fcb1aff6b0d47d812988c31b0cb5e777ece
MahitMehta <73650235+MahitMehta@users.noreply.github.com>
2021-04-15 23:05:02 -0400

    Client Path Fixes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const payment = require('./routes/payment')
 
-app.use(express.static(__dirname + "\\client\\build"))
+app.use(express.static(__dirname + "/client/build"))
 app.use("/api/payment", payment);
 
 app.get('*', (_, res) => {
-    res.sendFile(__dirname + "\\client\\build\\index.html")
+    res.sendFile(__dirname + "/client/build/index.html")
 })
 
 const PORT = process.env.PORT || 5000
